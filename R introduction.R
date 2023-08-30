be006cf632b09d865595ca63d4db4d62aace06c4
omidgr8 <143612109+omidgr8@users.noreply.github.com>
2023-08-30 17:50:37 +0530

    Add files via upload

---
+++ R introduction.R
@@ -0,0 +1,387 @@
+"hello world!"
+'hello world!'
+hello 
+hello
+#to output number
+5
+15
+
+
+
+5  10  15  200 #will raise error
+
+# variable
+
+name1 = "AMIT"
+name = "yash"
+Name3 = "omi"
+name1 = "sumit"
+name3 = "1520"
+name1
+age = 125
+age1 = 252
+age
+Age
+num = "20"
+age = age=15
+num=num+num+5
+age=age+age+age+5
+4*83
+15151515*123456789
+text="excelent"
+paste("excelr is",text)
+paste("Name3 is",text)
+paste(Name3,"is",text)
+paste(name,"is",text)
+paste(age,age1,name,name1,name3,Name3,"are",text)
+
+
+var1 =var2 =var3 = "omi d gr8"
+#legal variable name
+myvar="sumya"
+my_var="amya"
+myVar="kamya"
+myvar2="samya"
+this.year=2023
+
+#illegal variable name
+2myvar="s"
+my-var="b"
+my var="k"
+_my_var="5"
+my_v@var="s"
+TRUE = "s"
+s
+TRUE
+TRUE=
+mygirlfriend ="yash"
+mygirlfriend="yash"
+
+
+x=10.5
+class(x)
+class(name)
+
+x="r is exciting"
+class(x)
+x = TRUE   # or t,false, f
+x=F
+
+#comparision operator
+3>5
+5>3
+5==5
+1!=6
+!= is used for not equal to
+
+
+#arithmatic operator
+x=20
+y=3
+x*y
+x+y
+x-y
+x/y
+y-x
+x^2
+x%%y #modulas
+x%/%y #integer division
+
+
+2*pi
+pi
+pipipi
+pi pi pi
+??Constants
+LETTERS
+month.name
+capabilities(25)
+50->x
+205->x
+max(15,25,88952558)
+min(252582,0183048,018520158502)
+sqrt(625)
+abs(-1)
+
+
+#deta structure:vectors,dataframe
+#homogeneous vectors
+#vector of strings
+fruits="banana"
+fruits=c("banana","apple","orange","strawberry")
+class(fruits)
+n1=C(1,2,3)
+marks=c(1,2,3)
+n2=c(T,F,TRUE,FALSE)
+class(n2)
+
+
+#heterogeneous vectors
+mix=c(81,5.2,TRUE,5L,F)
+
+
+
+x=c(2,5,8,4)
+y=c(1,9,9,9)
+
+x+y
+x*5
+
+
+
+#sequence
+
+1:10
+1:1800
+1000:5000
+
+
+seq(1,10000,15)#start value,end value,step/increment value
+
+seq(1,12000,by=20)
+seq(1,-25000,-25)
+var
+name)
+"name"
+paste("name+name1")
+paste("name")
+paste(name)
+paste(var1,"is",text)
+paste("omkar is the besd")
+seq(1,-235000,-15)
+seq(-1000,-36987412,-15)
+n1=1.5:6.5
+
+
+
+#repetition
+rep(45,7)
+rep("omi D GR8",200000)
+
+
+
+#RANDOM SAMPLE
+sample(1:50,3)
+sample(1:50,15)
+sample(1:10,200)
+sample(1:10,200,replace=TRUE)
+s
+
+sample(c("HP","ORANGE","OMKAR"),15,replace=T)
+
+
+sample(c("omkar","is","gr8"),90,replace=T)
+
+
+sample(c("omkar","patil","karad"),2)
+
+
+
+
+#indexing/ Accessing vector elements
+
+x=c(2,5,8,20,10,30,58)
+
+
+x[4]
+
+
+x[8]
+
+
+x[5]
+
+
+x[c(4,5)]
+x[-1]
+
+x[-8]
+x[-2]
+x[c(-2,-5,-7)]
+x[c(-1,-2,-3,-4,-5,-6,-7)]
+x[-7]
+
+
+
+x[1]=3
+x[-1]=5
+x[-3]=9
+x[c(-2,-7)]=6
+x[4]=9
+x[5]=0
+sample(x[c(1,2)],1000,replace = T)
+
+
+
+y<9
+
+
+
+y[y<9]=7
+
+
+
+y[y>7]=10
+
+
+y[y<5]=8
+
+
+
+
+
+#relational operators with vectors
+
+
+marks=c(65,85,55,35,95)
+
+marks>50
+
+marks>55
+
+
+
+names=c("snehal","pooja","vidya","ganesh")
+
+p1=c('a','b')
+
+
+
+
+"pooja"%in%names
+
+
+"omkar"%in%names
+
+
+"Pooja"%in%names
+
+
+"pooja"%out%names
+
+
+#slicing
+
+
+
+marks
+
+
+marks[3:7]
+
+
+marks[5]
+
+
+marks[3]=69
+marks[-3]=69
+
+
+
+
+
+price=c(2999,499,29,650,2800,192,9384,373,474,4745)
+
+
+price[price>1000]
+
+
+
+sort(price)
+
+
+sort(marks)
+
+
+
+
+sort(price,decreasing=T)
+
+
+help("sort")
+
+
+
+help("mean")
+
+
+length(marks)
+
+
+paste(1:1200)
+
+
+
+
+
+paste(1:12)
+
+
+
+nth=paste(1:12,c("st","nd","rd",rep("th",9)))
+nth
+paste(1:100)
+
+nth=paste(1:100,c("st","nd","rd",rep("th",96),"rd"))
+nth
+
+
+
+
+
+month.name
+
+
+month.abb
+
+
+
+paste(month.abb,"is the",nth,"month of the year")
+
+
+#basic functions on vectors
+
+price
+length(price)
+
+max(price)
+
+min(price)
+
+
+sum(price)
+
+
+
+mean(price)
+
+
+median(price)
+
+
+help(mode)
+
+mode(price)
+
+
+
+
+#data frames
+#slicing the data frames
+
+a=c(42,18,91,87,66)
+
+b=c("p","q","r","s","t")
+
+data.frame(a,b)
+
+df=data.frame(a,b)
+
+
+
+
+df1=data.frame(
+  training=c("strenghth","stamina","other"),
+  pulse=c(100,150,120),
+  duration=c(60,30,45)
+)
